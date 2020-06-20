cabc66ea04dab726d5ce4866160def55eb8fcf5f
Xecto <muench.alex.90@gmail.com>
2020-06-20 18:54:53 +0200

    update

--- algo.py
+++ algo.py
@@ -6,6 +6,17 @@ ret = Bars_Helper.get_Bar_Price("AAPL", limit=15)
 
 Assets = Bars_Helper.get_Asset_List()
 
-while True:
-    for item in Assets:
-        print(Bars_Helper.sanalyze_Bar_Price(item[0]))
+symbols = Bars_Helper.get_Asset_List()
+list_symbols = []
+for counter, symbol in enumerate(symbols):
+    list_symbols.append(symbol[0])
+    if counter>0:
+        break
+
+from datetime import datetime, timedelta
+past = str(datetime.today() - timedelta(days=100)).replace(" ","T")[:19]+"-01:00"
+list_symbols = ["AAPL", "GOOGL"]
+tmp = []
+for i in range(7):
+    today=str(datetime.today() - timedelta(days=i)).replace(" ","T")[:19]+"-01:00"
+    #tmp.append(MACD_Class.calculate(list_symbols, past,today))
--- functions.py
+++ functions.py
@@ -110,21 +110,3 @@ class MACD_Class:
             ret.append(tmp)
 
         return ret
-
-
-
-
-symbols = Bars_Helper.get_Asset_List()
-list_symbols = []
-for counter, symbol in enumerate(symbols):
-    list_symbols.append(symbol[0])
-    if counter>0:
-        break
-
-from datetime import datetime, timedelta
-past = str(datetime.today() - timedelta(days=100)).replace(" ","T")[:19]+"-01:00"
-list_symbols = ["AAPL", "GOOGL"]
-tmp = []
-for i in range(7):
-    today=str(datetime.today() - timedelta(days=i)).replace(" ","T")[:19]+"-01:00"
-    #tmp.append(MACD_Class.calculate(list_symbols, past,today))
